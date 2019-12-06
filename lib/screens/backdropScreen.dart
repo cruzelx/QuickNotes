d1baeb06a84252f2315f2ffdfbe83512efe0a942
cruzelx <cruze.alex@gmail.com>
2019-12-06 14:35:53 +0545

    backdrop drom flutter_gallary add

--- lib/screens/backdropScreen.dart
+++ lib/screens/backdropScreen.dart
@@ -1,5 +1,6 @@
 import 'package:backdrop/backdrop.dart';
 import 'package:flutter/material.dart';
+import 'package:quicknotes/screens/twoPanels.dart';
 import 'package:quicknotes/widgets/backLayerNote.dart';
 
 class BackDropScreen extends StatefulWidget {
@@ -7,26 +8,59 @@ class BackDropScreen extends StatefulWidget {
   _BackDropScreenState createState() => _BackDropScreenState();
 }
 
-class _BackDropScreenState extends State<BackDropScreen> {
+class _BackDropScreenState extends State<BackDropScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  String title = "Add Note";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+      value: 1.0,
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _animationController.dispose();
+  }
+
+  bool get isBackVisible {
+    final AnimationStatus status = _animationController.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BackdropScaffold(
-      title: Text("Title"),
-      headerHeight: MediaQuery.of(context).size.height*0.5,
-      frontLayer: Center(
-        // child: Scaffold(
-        //   floatingActionButton: FloatingActionButton.extended(
-        //     icon: Icon(Icons.add),
-        //     label: Text("add"),
-        //   ),
-        ),
-        
-      
-      backLayer: Center(
-        child: BackdropMenu(),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text(title),
+        actions: <Widget>[
+          IconButton(
+            icon: AnimatedIcon(
+              icon: AnimatedIcons.close_menu,
+              progress: _animationController.view,
+            ),
+            onPressed: () {
+              setState(() {
+                title = "Menu";
+              });
+              _animationController.fling(
+                velocity: isBackVisible ? -1.0 : 1.0,
+              );
+            },
+          ),
+        ],
       ),
-      iconPosition: BackdropIconPosition.action,
-
+      body: TwoPanels(animationController: _animationController,),
     );
   }
 }
---
+++ lib/screens/twoPanels.dart
@@ -0,0 +1,109 @@
+import "package:flutter/material.dart";
+import 'dart:math' as math;
+
+class TwoPanels extends StatefulWidget {
+  final AnimationController animationController;
+  TwoPanels({this.animationController});
+  @override
+  _TwoPanelsState createState() => _TwoPanelsState();
+}
+
+class _TwoPanelsState extends State<TwoPanels>
+    with SingleTickerProviderStateMixin {
+  //final GlobalKey _backdropKey = GlobalKey();
+  static const header_height = 40.0;
+
+  Animation<RelativeRect> getPanelAnimation(BoxConstraints constraints) {
+    final height = constraints.biggest.height;
+    final backPanelHeight = height - header_height;
+    final frontPanelHeight = -header_height;
+
+    return RelativeRectTween(
+      begin: RelativeRect.fromLTRB(0.0, backPanelHeight, 0.0, frontPanelHeight),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(CurvedAnimation(
+        parent: widget.animationController, curve: Curves.easeOut));
+  }
+
+  void isBackVisible() {
+    final AnimationStatus status = widget.animationController.status;
+    final bool isOpen = status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+    widget.animationController.fling(velocity: isOpen ? -1.0 : 1.0);
+  }
+
+  // void _handleDragUpdate(DragUpdateDetails details) {
+  //   final RenderBox renderBox =
+  //       _backdropKey.currentContext.findRenderObject() as RenderBox;
+  //   final _backdropHeight = math.max(0.0, renderBox.size.height - 56.0 - 92.0);
+  //   widget.animationController.value -=
+  //       details.primaryDelta / (_backdropHeight ?? details.primaryDelta);
+  // }
+
+  // void _handleDragEnd(DragEndDetails details) {
+  //   if (widget.animationController.isAnimating ||
+  //       widget.animationController.status == AnimationStatus.completed) return;
+  // }
+
+  Widget bothPanels(BuildContext context, BoxConstraints constraints) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Text(
+                "backPanel",
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+            ),
+          ),
+          PositionedTransition(
+            rect: getPanelAnimation(constraints),
+            child: Material(
+              elevation: 15.0,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16.0),
+                topRight: Radius.circular(16.0),
+              ),
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                   onTap: isBackVisible,
+                    child: SizedBox(
+                      height: header_height,
+                      width: double.infinity,
+                      child: Container(
+                        color: Colors.pink,
+                        width: double.infinity,
+                        height: header_height,
+                        child: Center(
+                          child: Text(
+                            "frontPanelHeader",
+                            style: Theme.of(context).textTheme.button,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text("frontPanel"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: bothPanels,
+    );
+  }
+}
